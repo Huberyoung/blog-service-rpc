cb653d730f41f913fcd0a7411df6a90c13d4ad6b
huberyyang <yangcai@4399inc.com>
2023-03-27 22:27:02 +0800

    feat:grpc-gateway

--- go.mod
+++ go.mod
@@ -3,17 +3,24 @@ module blog_service_grpc
 go 1.19
 
 require (
-	cloud.google.com/go/compute v1.18.0 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/fullstorydev/grpcurl v1.8.7 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2
+	github.com/soheilhy/cmux v0.1.4
+	google.golang.org/grpc v1.54.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/golang/glog v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/jhump/protoreflect v1.12.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.4.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
-	google.golang.org/grpc v1.53.0 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
+	google.golang.org/genproto v0.0.0-20230323212658-478b75c54725 // indirect
+	google.golang.org/grpc/examples v0.0.0-20230323235726-c018273e5353 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,31 +4,65 @@ import (
 	"blog_service_grpc/proto/pb"
 	"blog_service_grpc/server"
 	"flag"
+	"github.com/soheilhy/cmux"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 )
 
+var grpcPort string
+var httpPort string
 var port string
 
 func init() {
-	flag.StringVar(&port, "p", "8001", "启动端口号")
+	//flag.StringVar(&grpcPort, "grpc_port", "8001", "grpc 启动端口号")
+	//flag.StringVar(&httpPort, "http_port", "9001", "http 启动端口号")
+	flag.StringVar(&port, "http_port", "8003", "启动端口号")
 	flag.Parsed()
 }
 
-func main() {
+func RunTcpServer(port string) (net.Listener, error) {
+	return net.Listen("tcp", ":"+port)
+}
+
+func RunHttpServer(port string) *http.Server {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: serveMux,
+	}
+}
+
+func RunGrpcServer() *grpc.Server {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
+	return s
+}
 
-	listen, err := net.Listen("tcp", ":"+port)
+func main() {
+	l, err := RunTcpServer(port)
 	if err != nil {
-		log.Printf("net.Listen err: %v", err)
+		log.Fatalf("Run Tcp Server err:%v", err)
 	}
 
-	err = s.Serve(listen)
+	m := cmux.New(l)
+	grpcL := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
+	httpL := m.Match(cmux.HTTP1Fast())
+
+	grpcS := RunGrpcServer()
+	HttpS := RunHttpServer(port)
+
+	go grpcS.Serve(grpcL)
+	go HttpS.Serve(httpL)
+
+	err = m.Serve()
 	if err != nil {
-		log.Printf("s.Serve err: %v", err)
+		log.Fatalf("Run Server err:%v", err)
 	}
 }
